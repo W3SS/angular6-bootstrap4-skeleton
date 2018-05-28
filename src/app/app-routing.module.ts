d0a1c8f6b6dca5a774f455af980bbd15f6cc6766
Wagner Landim <wagner.landim@gmail.com>
2018-05-28 15:18:33 -0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'teste', component: AppComponent },
+  { path: '', redirectTo: '/cad/processos-seletivos', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
